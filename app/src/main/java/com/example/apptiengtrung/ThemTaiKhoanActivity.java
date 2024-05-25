f608d9293d379e409db4f9a06ae045c8f7495a5e
Kat <113763986+nhatlinh0@users.noreply.github.com>
2024-05-26 00:25:28 +0700

    Update ThemTaiKhoanActivity.java

--- app/src/main/java/com/example/apptiengtrung/ThemTaiKhoanActivity.java
+++ app/src/main/java/com/example/apptiengtrung/ThemTaiKhoanActivity.java
@@ -1,5 +1,4 @@
 package com.example.apptiengtrung;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,91 +8,58 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.lifecycle.ViewModelProvider;
 
 public class ThemTaiKhoanActivity extends AppCompatActivity {
-    int count;
-    boolean check;
-    boolean check1;
+    //    mvvm parttern
+    //    observe
+    private ThemTaiKhoanViewModel viewModel;
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private EditText edtDiem;
+    private boolean check;
+    private boolean check1;
+    private int count;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.themtaikhoan_layout);
 
-        count = 1;
-        EditText edtUsername = findViewById(R.id.edtUsername);
-        EditText edtPassword = findViewById(R.id.edtPassword);
-        EditText edtDiem = findViewById(R.id.edtDiem);
+        viewModel = new ViewModelProvider(this).get(ThemTaiKhoanViewModel.class);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtDiem = findViewById(R.id.edtDiem);
         CheckBox cbGiaoVien = findViewById(R.id.cbGiaoVien);
         CheckBox cbAdmin = findViewById(R.id.cbAdmin);
         ImageView ivBack = findViewById(R.id.ivBack);
         Button btnThem = findViewById(R.id.btnThem);
 
-        cbGiaoVien.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Perform action based on checkbox state
-                if (isChecked) {
-                    check = true;
-                } else {
-                    check = false;
-                }
-            }
-        });
+        cbGiaoVien.setOnCheckedChangeListener((buttonView, isChecked) -> check = isChecked);
 
-        cbAdmin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Perform action based on checkbox state
-                if (isChecked) {
-                    check1 = true;
-                } else {
-                    check1 = false;
-                }
-            }
-        });
+        cbAdmin.setOnCheckedChangeListener((buttonView, isChecked) -> check1 = isChecked);
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child("User").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot snap : snapshot.getChildren()) {
-                    count++;
-                }
-            }
+        viewModel.getCount().observe(this, newCount -> count = newCount);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-
-        });
-
-        ivBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        viewModel.getAddSuccess().observe(this, success -> {
+            if (success) {
                 Intent intent = new Intent(ThemTaiKhoanActivity.this, DanhSachTaiKhoanActivity.class);
                 startActivity(intent);
             }
         });
 
-        btnThem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String us = edtUsername.getText().toString();
-                String pw = edtPassword.getText().toString();
-                int score = Integer.parseInt(edtDiem.getText().toString());
-                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-                databaseReference.child("User").child(count+"").setValue(new User(count,us,pw,score,check,check1));
-                Intent intent = new Intent(ThemTaiKhoanActivity.this, DanhSachTaiKhoanActivity.class);
-                startActivity(intent);
-            }
+        ivBack.setOnClickListener(view -> {
+            Intent intent = new Intent(ThemTaiKhoanActivity.this, DanhSachTaiKhoanActivity.class);
+            startActivity(intent);
+        });
+
+        btnThem.setOnClickListener(view -> {
+            String us = edtUsername.getText().toString();
+            String pw = edtPassword.getText().toString();
+            int score = Integer.parseInt(edtDiem.getText().toString());
+            viewModel.addUser(us, pw, score, check, check1);
         });
     }
 }
