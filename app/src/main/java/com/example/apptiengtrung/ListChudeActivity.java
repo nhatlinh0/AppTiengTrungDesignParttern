26d74f8d04ea1cfffa440a7633babc46ab4e6004
Kat <113763986+nhatlinh0@users.noreply.github.com>
2024-05-26 00:29:21 +0700

    Update ListChudeActivity.java

--- app/src/main/java/com/example/apptiengtrung/ListChudeActivity.java
+++ app/src/main/java/com/example/apptiengtrung/ListChudeActivity.java
@@ -3,74 +3,62 @@ package com.example.apptiengtrung;
 import android.os.Bundle;
 import android.view.View;
 
-//import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 
 import com.example.apptiengtrung.databinding.ActivityListChudeBinding;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ListChudeActivity extends BaseActivity {
-    ActivityListChudeBinding binding;
+
+//    mvvm parttern
+
+    private ActivityListChudeBinding binding;
+    private ListChudeViewModel viewModel;
     private int chudeId;
     private String categoryName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityListChudeBinding.inflate(getLayoutInflater());
+        binding = ActivityListChudeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         getIntentUltra666();
-        innitList();
-
+        initViewModel();
+        setupObservers();
+        viewModel.fetchListChude(chudeId);
     }
 
-    private void innitList() {
-        DatabaseReference myRef=database.getReference("CaunoiId");
-        binding.progressBar.setVisibility(View.VISIBLE);
-        ArrayList<ListChude>list=new ArrayList<>();
-        Query query=myRef.orderByChild("ChudeId").equalTo(chudeId);
+    private void initViewModel() {
+        viewModel = new ViewModelProvider(this).get(ListChudeViewModel.class);
+    }
 
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+    private void setupObservers() {
+        viewModel.getListChude().observe(this, new Observer<ArrayList<ListChude>>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    for(DataSnapshot issue:snapshot.getChildren()){
-                        list.add(issue.getValue(ListChude.class));
-                    }
-                    if(list.size()>0){
-                        binding.chudelistView.setLayoutManager(new GridLayoutManager(ListChudeActivity.this, LinearLayoutManager.VERTICAL));
-                        binding.chudelistView.setAdapter(new ListChudeAdapter(list));
-                    }
-                    binding.progressBar.setVisibility(View.GONE);
+            public void onChanged(ArrayList<ListChude> listChudes) {
+                if (listChudes != null) {
+                    setupRecyclerView(listChudes);
                 }
                 binding.progressBar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
     }
 
+    private void setupRecyclerView(ArrayList<ListChude> list) {
+        binding.chudelistView.setLayoutManager(new GridLayoutManager(this, LinearLayoutManager.VERTICAL));
+        binding.chudelistView.setAdapter(new ListChudeAdapter(list));
+    }
+
     private void getIntentUltra666() {
-        chudeId=getIntent().getIntExtra("ChudeId",1);
-        categoryName=getIntent().getStringExtra("CategoryName");
+        chudeId = getIntent().getIntExtra("ChudeId", 1);
+        categoryName = getIntent().getStringExtra("CategoryName");
 
         binding.titleTxt.setText(categoryName);
         binding.Backbtn.setOnClickListener(v -> finish());
